f75b9490d59287d84658c38293b5e3a7a70d887f
Ivan Towlson <ivan.towlson@microsoft.com>
2017-08-16 17:09:24 +1200

    Transfer commit of additional tests

--- pkg/api/validate_test.go
+++ pkg/api/validate_test.go
@@ -46,6 +46,30 @@ func Test_OrchestratorProfile_DCOS_UnsupportedReleasesFailValidation(t *testing.
 	}
 }
 
+func Test_OrchestratorProfile_Kubernetes_SupportedReleasesOK(t *testing.T) {
+	for _, r := range []string{common.KubernetesRelease1Dot5, common.KubernetesRelease1Dot6, common.KubernetesRelease1Dot7} {
+		o := &OrchestratorProfile{
+			OrchestratorType:    "Kubernetes",
+			OrchestratorRelease: r,
+		}
+		if err := o.Validate(); err != nil {
+			t.Errorf("should not error with orchestrator type '%s' and release '%s'", o.OrchestratorType, o.OrchestratorRelease)
+		}
+	}
+}
+
+func Test_OrchestratorProfile_Kubernetes_UnsupportedReleasesFailValidation(t *testing.T) {
+	for _, r := range []string{"1.1"} {
+		o := &OrchestratorProfile{
+			OrchestratorType:    "Kubernetes",
+			OrchestratorRelease: r,
+		}
+		if err := o.Validate(); err == nil {
+			t.Errorf("should error with orchestrator type '%s' and release '%s'", o.OrchestratorType, o.OrchestratorRelease)
+		}
+	}
+}
+
 func Test_OrchestratorProfile_FailsWhenKubernetesConfigPopulatedForNonKubernetesOrchestrator(t *testing.T) {
 	for _, ot := range []string{Swarm, SwarmMode, DCOS, DockerCE} {
 		o := &OrchestratorProfile{
